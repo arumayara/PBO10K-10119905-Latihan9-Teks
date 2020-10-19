bf0c0b47f13168c3d7c42d062aa81d08a20cc007
arumayara <72974847+arumayara@users.noreply.github.com>
2020-10-19 07:15:56 +0700

    Add files via upload

---
+++ PBO10K-10119905-Latihan9-Teks/src/main/java/PBO10K/NIM10119905/Latihan9/Teks/PBO10K10119905Latihan9.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PBO10K.NIM10119905.Latihan9.Teks;
+
+/**
+ *
+ * @author 
+ * NAMA    : Abraham Rumayara
+ * KELAS   : PBO10K
+ * NIM     : 10119905
+ * Deskripsi Program : Program ini menampilkan tipe data Teks
+ * 
+ */
+public class PBO10K10119905Latihan9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        char c;
+        char tab ='\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("Hasil"+ newline + "Char: "+ tab +"berisi "+ c);
+    }
+    
+}
